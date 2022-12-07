bca91780cee16118f8d6badd23d6540f131d69e7
Martín Morici <martinmorici3@gmail.com>
2022-12-07 18:10:22 -0300

    inicio proyecto ecommerce

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from 'react-router-dom';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Product from './pages/Home';
+import Category from './pages/Home';
+
+const SharedLayout = () => {
+  return (
+    <div className='App'>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <SharedLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/category/:id',
+        element: <Category />,
+      },
+      {
+        path: '/product/:id',
+        element: <Product />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div className='App'>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
